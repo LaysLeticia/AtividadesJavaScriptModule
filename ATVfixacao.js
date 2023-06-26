b26a50dc1ac9aadc1908cec9470f51747caaf378
LaysLeticia <136758166+LaysLeticia@users.noreply.github.com>
2023-06-26 20:25:11 -0300

    Add files via upload
    Atividade 26/06 Tquarta

---
+++ ATVfixacao.js
@@ -0,0 +1,30 @@
+//atividade fixacao 1
+//UsandoVar
+var nota1 = 10;
+var nota2 = 7;
+var total = nota1 + nota2;
+console.log(total);
+
+var a = 3;
+var b = 5;
+var c = a * b;
+console.log(c);
+ 
+//UsandoLet
+let nomeCompleto = "Lays Leticia de Lima Silva";
+console.log(nomeCompleto);
+
+let mensagem = "Bem Vindos, alunos Front  2023";
+console.log(mensagem);
+
+//UsandoConst
+const CPF = 300400506;
+console.log(CPF);
+
+const PI = 15698;
+console.log(PI);
+
+
+
+
+
---
+++ Untitled-1.js
@@ -0,0 +1,52 @@
+// 14 variaveis
+//14variaveis
+//tipoNumber
+var idade = 25;
+console.log(idade);
+
+var numero_inteiro = 9;
+console.log(numero_inteiro);
+
+//tipoString
+var nome = 'Lays';
+console.log(nome);
+
+var sua_cidade = "Olinda";
+console.log(sua_cidade);
+
+//tipoBoolean
+var verdadeiro = true;
+var falso = false; 
+
+console.log("Voce está estudando?");
+console.log(verdadeiro);
+console.log("E voce não está aprendendo.");
+console.log(falso);
+
+//tipoArray
+var dias_da_semana = ["seg", "ter", "qua", "qui", "sex"];
+console.log(dias_da_semana);
+
+var tiposEspelhos = ["Pequeno", "Médio", "Grande"];
+console.log(tiposEspelhos);
+
+//tipoObject
+var inscricao= {ano: "2023", turma: "Front"};
+console.log(inscricao.ano);
+console.log(inscricao.turma);
+
+var dna = {cor_olho: "verde", mulher: true};
+console.log(dna.cor_olho);
+console.log(dna.mulher);
+
+//tipoUndefined&null
+var valor_nao_definido ;
+console.log("valor_nao_definido" + valor_nao_definido);
+var valor_vazio
+console.log("valor_vazio" + valor_vazio);
+
+
+
+
+
+
